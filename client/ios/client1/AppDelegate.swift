524d3831dc9e2617db236e6e1f769c06147a2656
Komal Patel <komal.makwana@aristomax.com>
2025-09-03 16:24:41 +0530

    camera functionality added and client1 replaced everywhere

--- client/ios/client1/AppDelegate.swift
+++ client/ios/client1/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     window = UIWindow(frame: UIScreen.main.bounds)
 
     factory.startReactNative(
-      withModuleName: "client1",
+      withModuleName: "client",
       in: window,
       launchOptions: launchOptions
     )
