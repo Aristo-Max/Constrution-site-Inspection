0a5f249d688b8923e46a44a36751ccd12702351a
Komal Patel <komal.makwana@aristomax.com>
2025-09-05 17:31:38 +0530

    minor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { NavigationContainer} from '@react-navigation/native';
+import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { View, Text } from 'react-native';
 import Login from './screens/Login';
@@ -10,6 +10,7 @@ import DashboardScreen from './screens/DashboardScreen';
 import ReportScreen from './screens/ReportScreen';
 import { Inspection, NoteItem, PropertyProvider } from './components/PropertyContext';
 import NewInspectionScreen from './screens/NewInspection';
+import colors from './constants/color';
 
 // import View from 'react-native/types_generated/Libraries/Components/View/View';
 
@@ -18,7 +19,7 @@ const Stack = createNativeStackNavigator<RootStackParamList>();
 export type RootStackParamList = {
   Splash: undefined;
   Login: undefined;
-    Dashboard: {
+  Dashboard: {
     updatedNote?: NoteItem;
     propertyId?: string;
   } | undefined;
@@ -38,49 +39,56 @@ const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [permissionsGranted, setPermissionsGranted] = useState(false);
   const [splashDone, setSplashDone] = useState(false);
-// console.log("Splash Done:", splashDone);
-// console.log("Permissions Granted:", permissionsGranted);
-// console.log("Is Logged In:", isLoggedIn);
+  // console.log("Splash Done:", splashDone);
+  // console.log("Permissions Granted:", permissionsGranted);
+  // console.log("Is Logged In:", isLoggedIn);
   return (
     //  <View style={{flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: 'lightblue'}}>
     //   <Text style={{fontSize: 20}}>Hello, React Native!</Text>
     // </View>
     <PropertyProvider>
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        
-        {/* Always show splash first */}
-         {!splashDone ? (
-          <Stack.Screen name="Splash">
-            {(props) => (
-              <SplashScreen {...props} onFinish={() => setSplashDone(true)} />
-            )}
-          </Stack.Screen>
-        ) :!isLoggedIn ? (
-          <Stack.Screen name="Login">
-            {(props) => (
-              
-              <Login {...props} onLogin={() => setIsLoggedIn(true)} />
-            )}
-          </Stack.Screen>
-        ) : (
-          <Stack.Screen name="Dashboard" component={DashboardScreen} />
+      <NavigationContainer>
+        <Stack.Navigator screenOptions={{ headerShown: false }}>
 
-        )}
-        <Stack.Screen name="InspectionForm" component={InspectionForm}  />
-         <Stack.Screen name="NewInspection" component={NewInspectionScreen}  options={{
-    title: "Inspection Form",
-    headerShown: true, // show the header
-    headerTintColor: "#fff",
-    headerStyle: { backgroundColor: "#0a84ff" },
-  }} />
-         <Stack.Screen name="ReportScreen" component={ReportScreen} options={{
-  title: "ReportScreen",
-  headerTintColor: "#fff",
-  headerStyle: { backgroundColor: "#0a84ff" },
-}}/> 
-      </Stack.Navigator>
-    </NavigationContainer>
+          {/* Always show splash first */}
+          {!splashDone ? (
+            <Stack.Screen name="Splash">
+              {(props) => (
+                <SplashScreen {...props} onFinish={() => setSplashDone(true)} />
+              )}
+            </Stack.Screen>
+          ) : !isLoggedIn ? (
+            <Stack.Screen name="Login">
+              {(props) => (
+
+                <Login {...props} onLogin={() => setIsLoggedIn(true)} />
+              )}
+            </Stack.Screen>
+          ) : (
+            <Stack.Screen name="Dashboard" component={DashboardScreen} />
+
+          )}
+          <Stack.Screen name="InspectionForm" component={InspectionForm}
+            options={{
+              title: "Inspection Form",
+              headerShown: true,
+              headerTintColor: "#fff",
+              headerStyle: { backgroundColor: colors.primary },
+            }} />
+          <Stack.Screen name="NewInspection" component={NewInspectionScreen} options={{
+            title: "New Inspection",
+            headerShown: true, // show the header
+            headerTintColor: "#fff",
+            headerStyle: { backgroundColor: colors.primary },
+          }} />
+          <Stack.Screen name="ReportScreen" component={ReportScreen} options={{
+            title: "ReportScreen",
+            headerShown: true,
+            headerTintColor: "#fff",
+            headerStyle: { backgroundColor: colors.primary },
+          }} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </PropertyProvider>
   );
 };
